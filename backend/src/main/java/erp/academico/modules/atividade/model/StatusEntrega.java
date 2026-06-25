9143d0449386b9bfdde27d4a28acf37ea4edf076
devlucasaf <freitas.lucasaf@gmail.com>
2026-06-25 17:12:45 -0300

    feat(atividade): adiciona modulo de atividades e entregas

---
+++ backend/src/main/java/erp/academico/modules/atividade/model/StatusEntrega.java
@@ -0,0 +1,9 @@
+package erp.academico.modules.atividade.model;
+
+public enum StatusEntrega {
+    PENDENTE,
+    ENTREGUE,
+    AVALIADA,
+    ATRASADA
+}
+
---
+++ backend/src/main/java/erp/academico/modules/atividade/model/TipoAtividade.java
@@ -0,0 +1,9 @@
+package erp.academico.modules.atividade.model;
+
+public enum TipoAtividade {
+    TAREFA,
+    TRABALHO,
+    PROJETO,
+    PROVA
+}
+
